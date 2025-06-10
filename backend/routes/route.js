9593f66700daccc00ed4a1d2848529e7ecdb2805
Asadullah <129300889+Asadullah-byte@users.noreply.github.com>
2025-06-10 09:19:50 +0500

    edited routes and quering spotify for nasheed related to mood

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { handleInput, handleLogin,handleCallback } from "../controllers/moodController.js";
+import { handleInput, handleLogin,handleCallback,handleGetNasheeds } from "../controllers/moodController.js";
+import { ensureSpotifyToken } from "../middleware/spotifyAuth.js";
 
 const router = Router();
 
-router.get("/login", handleLogin)
-router.get("/callback", handleCallback)
-router.post("/input", handleInput);
-
+router.get("/api/login", handleLogin)
+router.get("/api/callback", handleCallback)
+router.post("/api/input",ensureSpotifyToken, handleInput);
+router.get("/api/nasheeds",ensureSpotifyToken, handleGetNasheeds)
 
 export default router;
